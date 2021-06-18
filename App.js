b1e169097a118bb986eac5f2d6a24e54319f4c92
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-06-18 11:19:06 +0900

    navigators WIP

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import LoggedOutNav from "./navigators/LoggedOutNav";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require("./assets/instagram_logo.png"),
+      "https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg",
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>WOW</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
